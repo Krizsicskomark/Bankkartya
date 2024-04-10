d5b6edd070632c5b5eff558c9ae906cdf3132fae
Krizsicskomark <151917177+Krizsicskomark@users.noreply.github.com>
2024-04-10 10:07:24 +0200

    Add files via upload

---
+++ Bankkartya/Bankkartya/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+public class Bankkartya
+{
+    public string OwnerName { get; set; }
+    public string CardNumber { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public decimal Balance { get; set; }
+
+    public Bankkartya(string ownerName, string cardNumber, DateTime expirationDate, decimal balance)
+    {
+        OwnerName = ownerName;
+        CardNumber = cardNumber;
+        ExpirationDate = expirationDate;
+        Balance = balance;
+    }
+
+    public void PenzKivetel(decimal amount)
+    {   if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine($"{amount} FT sikeresen felvéve. Új egyenleg:{Balance} FT");
+        }
+        else
+        {
+            Console.WriteLine("Nem megfelelő összeg vagy nincs elegendő egyenleg a számlán");
+        }
+    }
+    public void PrintCardInfo()
+    {
+        Console.WriteLine("Bankkártya adatai:");
+        Console.WriteLine($"Tulajdonos neve: {OwnerName}");
+        Console.WriteLine($"Kártyaszám: {CardNumber}");
+        Console.WriteLine($"Lejárati dátum: {ExpirationDate:yyyy-MM-dd}");
+        Console.WriteLine($"Egyenleg: {Balance} Ft");
+    }
+
+}
+
